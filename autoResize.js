7b8f1d17f59428ef3dab34f56acaa4cbd7f2fa7f
gitrhs <48622808+gitrhs@users.noreply.github.com>
2023-12-26 02:12:58 +0800

    Update autoResize.js

--- autoResize.js
+++ autoResize.js
@@ -1,1 +1,32 @@
-
+import React, { useState, useEffect } from 'react';
+const autoResize = () => {
+  const images = [
+      'URL1', //image 1
+      'URL2', //image 2
+      //add another image here
+    ];
+  const [aspectRatios, setAspectRatios] = useState(images.map(() => 1));
+  useEffect(() => {
+    Promise.all(images.map(imageUrl => new Promise((resolve, reject) => {
+      Image.getSize(imageUrl, (width, height) => {
+        resolve(width / height);
+      }, reject);
+    }))).then(ratios => {
+      setAspectRatios(ratios);
+    });
+   }, []);
+  return (
+    {images.map((imageUrl, index) => (
+     <View key={index}>
+       <Image
+         style={{
+           width: '100%',
+           aspectRatio: aspectRatios[index],
+         }}
+         resizeMode={'contain'}
+         source={{ uri: imageUrl }}
+       />
+     </View>
+   ))}
+  );
+};
